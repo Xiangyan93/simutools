fe1dbbce4838f793ba9088ead029cf29db979243
xiangyan93 <yan.xiang@duke.edu>
2023-05-14 23:48:16 -0400

    updates.

--- scripts/EasySim.py
+++ scripts/EasySim.py
@@ -83,7 +83,7 @@ def main(args: CommonArgs):
                                               f'{TEMPLATE_DIR}/martini_v3.0.0_solvents_v1.itp',
                                               f'{TEMPLATE_DIR}/martini_v3.0.0_ions_v1.itp'] + itp_list,
                      mol_name=args.mol_name, mol_number=args.n_mol_list)
-    gmx.generate_mdp_from_template('t_CG_em.mdp', mdp_out=f'CG_em.mdp', dielectric=1.0)
+    gmx.generate_mdp_from_template('t_CG_em.mdp', mdp_out=f'CG_em.mdp', dielectric=15.0)
     gmx.generate_mdp_from_template('t_npt.mdp', mdp_out=f'CG_eq.mdp', nsteps=500000, dt=0.005, nstxtcout=10000,
                                    tcoupl='v-rescale', tau_t='1.0',
                                    pcoupl='berendsen', tau_p='1.0', compressibility='3e-4',
