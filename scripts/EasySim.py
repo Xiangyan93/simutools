1124e5ca0aeb538ac94fe0292861ec60cb85706b
xiangyan93 <yan.xiang@duke.edu>
2023-05-20 23:05:52 -0400

    automatically rebuilding the box if failed equilibrium.

--- scripts/EasySim.py
+++ scripts/EasySim.py
@@ -50,6 +50,8 @@ class CommonArgs(Tap):
     """"""
     PME: bool = False
     """"""
+    n_try: int = 10
+    """"""
     def process_args(self) -> None:
         assert len(self.box_size) == 3
         valid_index= []
@@ -63,59 +65,75 @@ class CommonArgs(Tap):
 
 def main(args: CommonArgs):
     cd_and_mkdir(args.save_dir)
-    if not os.path.exists('bulk.gro'):
-        pdb_files = []
-        for gro in args.gro_list:
-            pdb = gro.split('/')[-1].split('.')[0] + '.pdb'
-            universe = gro2pdb('../' + gro, pdb)
-            assert len(universe.residues) == 1
-            pdb_files.append(pdb)
-        packmol = Packmol('packmol')
-        packmol.build_box(pdb_files=pdb_files, n_mol_list=args.n_mol_list, output='bulk.pdb', box_size=args.box_size,
-                          tolerance=6.0)
-        pdb2gro('bulk.pdb', 'bulk.gro', [x * 10 for x in args.box_size] + [90., 90., 90.])
-    gmx = GROMACS(gmx_exe_mdrun='gmx')
-    itp_list = []
-    for itp in args.itp_list:
-        shutil.copy('../%s' % itp, '.')
-        itp_list.append(itp.split('/')[-1])
-    gmx.generate_top(f'CG.top', include_itps=[f'{TEMPLATE_DIR}/martini_v3.0.0.itp',
-                                              f'{TEMPLATE_DIR}/martini_v3.0.0_solvents_v1.itp',
-                                              f'{TEMPLATE_DIR}/martini_v3.0.0_ions_v1.itp'] + itp_list,
-                     mol_name=args.mol_name, mol_number=args.n_mol_list)
-    gmx.generate_mdp_from_template('t_CG_em.mdp', mdp_out=f'CG_em.mdp', dielectric=15.0)
-    gmx.generate_mdp_from_template('t_npt.mdp', mdp_out=f'CG_eq.mdp', nsteps=5000000, dt=0.005, nstxtcout=10000,
-                                   tcoupl='v-rescale', tau_t='1.0',
-                                   pcoupl='berendsen', tau_p='1.0', compressibility='3e-4',
-                                   constraints='none', coulombtype='cutoff',
-                                   rcoulomb='1.1', rvdw='1.1', dielectric=15, nstlist=20)
-    if args.PME:
-        gmx.generate_mdp_from_template('t_npt.mdp', mdp_out=f'CG_run.mdp', nsteps=5000000, dt=0.005, nstxtcout=10000,
-                                       restart=True,
-                                       tcoupl='v-rescale', tau_t='1.0',
-                                       pcoupl='parrinello-rahman', tau_p='12.0', compressibility='3e-4',
-                                       constraints='none', coulombtype='PME', rcoulomb='1.1',
-                                       rvdw='1.1', dielectric=15, nstlist=20)
-    else:
-        gmx.generate_mdp_from_template('t_npt.mdp', mdp_out=f'CG_run.mdp', nsteps=5000000, dt=0.005, nstxtcout=10000,
-                                       restart=True,
-                                       tcoupl='v-rescale', tau_t='1.0',
-                                       pcoupl='parrinello-rahman', tau_p='12.0', compressibility='3e-4',
-                                       constraints='none', coulombtype='reaction-field', rcoulomb='1.1',
-                                       rvdw='1.1', dielectric=15, nstlist=20)
-    if not os.path.exists('CG_em.gro'):
-        gmx.grompp(gro='bulk.gro', mdp='CG_em.mdp', top=f'CG.top', tpr=f'CG_em.tpr')
-        gmx.mdrun(tpr=f'CG_em.tpr', ntmpi=args.ntmpi, ntomp=args.ntomp)
-    if not os.path.exists('CG_eq.gro'):
-        gmx.grompp(gro=f'CG_em.gro', mdp='CG_eq.mdp', top=f'CG.top', tpr=f'CG_eq.tpr',
-                   maxwarn=2)
-        gmx.mdrun(tpr=f'CG_eq.tpr', ntmpi=args.ntmpi, ntomp=args.ntomp)
-    for i in range(args.n_runs):
-        if not os.path.exists(f'CG_run_{i}.gro'):
-            gro = 'CG_eq.gro' if i == 0 else f'CG_run_{i-1}.gro'
-            gmx.grompp(gro=gro, mdp='CG_run.mdp', top=f'CG.top', tpr=f'CG_run_{i}.tpr',
-                       maxwarn=1)
-            gmx.mdrun(tpr=f'CG_run_{i}.tpr', ntmpi=args.ntmpi, ntomp=args.ntomp)
+    for i in range(args.n_try):
+        try:
+            if not os.path.exists('bulk.gro'):
+                pdb_files = []
+                for gro in args.gro_list:
+                    pdb = gro.split('/')[-1].split('.')[0] + '.pdb'
+                    universe = gro2pdb('../' + gro, pdb)
+                    assert len(universe.residues) == 1
+                    pdb_files.append(pdb)
+                packmol = Packmol('packmol')
+                packmol.build_box(pdb_files=pdb_files, n_mol_list=args.n_mol_list, output='bulk.pdb', box_size=args.box_size,
+                                  tolerance=6.0)
+                pdb2gro('bulk.pdb', 'bulk.gro', [x * 10 for x in args.box_size] + [90., 90., 90.])
+            gmx = GROMACS(gmx_exe_mdrun='gmx')
+            itp_list = []
+            for itp in args.itp_list:
+                shutil.copy('../%s' % itp, '.')
+                itp_list.append(itp.split('/')[-1])
+            gmx.generate_top(f'CG.top', include_itps=[f'{TEMPLATE_DIR}/martini_v3.0.0.itp',
+                                                      f'{TEMPLATE_DIR}/martini_v3.0.0_solvents_v1.itp',
+                                                      f'{TEMPLATE_DIR}/martini_v3.0.0_ions_v1.itp'] + itp_list,
+                             mol_name=args.mol_name, mol_number=args.n_mol_list)
+            gmx.generate_mdp_from_template('t_CG_em.mdp', mdp_out=f'CG_em.mdp', dielectric=15.0)
+            gmx.generate_mdp_from_template('t_npt.mdp', mdp_out=f'CG_eq.mdp', nsteps=5000000, dt=0.005, nstxtcout=10000,
+                                           tcoupl='v-rescale', tau_t='1.0',
+                                           pcoupl='berendsen', tau_p='1.0', compressibility='3e-4',
+                                           constraints='none', coulombtype='cutoff',
+                                           rcoulomb='1.1', rvdw='1.1', dielectric=15, nstlist=20)
+            if args.PME:
+                gmx.generate_mdp_from_template('t_npt.mdp', mdp_out=f'CG_run.mdp', nsteps=5000000, dt=0.005, nstxtcout=10000,
+                                               restart=True,
+                                               tcoupl='v-rescale', tau_t='1.0',
+                                               pcoupl='parrinello-rahman', tau_p='12.0', compressibility='3e-4',
+                                               constraints='none', coulombtype='PME', rcoulomb='1.1',
+                                               rvdw='1.1', dielectric=15, nstlist=20)
+            else:
+                gmx.generate_mdp_from_template('t_npt.mdp', mdp_out=f'CG_run.mdp', nsteps=5000000, dt=0.005, nstxtcout=10000,
+                                               restart=True,
+                                               tcoupl='v-rescale', tau_t='1.0',
+                                               pcoupl='parrinello-rahman', tau_p='12.0', compressibility='3e-4',
+                                               constraints='none', coulombtype='reaction-field', rcoulomb='1.1',
+                                               rvdw='1.1', dielectric=15, nstlist=20)
+            if not os.path.exists('CG_em.gro'):
+                gmx.grompp(gro='bulk.gro', mdp='CG_em.mdp', top=f'CG.top', tpr=f'CG_em.tpr')
+                gmx.mdrun(tpr=f'CG_em.tpr', ntmpi=args.ntmpi, ntomp=args.ntomp)
+            if not os.path.exists('CG_eq.gro'):
+                gmx.grompp(gro=f'CG_em.gro', mdp='CG_eq.mdp', top=f'CG.top', tpr=f'CG_eq.tpr',
+                           maxwarn=2)
+                gmx.mdrun(tpr=f'CG_eq.tpr', ntmpi=args.ntmpi, ntomp=args.ntomp)
+            for i in range(args.n_runs):
+                if not os.path.exists(f'CG_run_{i}.gro'):
+                    gro = 'CG_eq.gro' if i == 0 else f'CG_run_{i-1}.gro'
+                    gmx.grompp(gro=gro, mdp='CG_run.mdp', top=f'CG.top', tpr=f'CG_run_{i}.tpr',
+                               maxwarn=1)
+                    gmx.mdrun(tpr=f'CG_run_{i}.tpr', ntmpi=args.ntmpi, ntomp=args.ntomp)
+        except:
+            if os.path.exists('CG_eq.gro'):
+                break
+            else:
+                current_directory = os.getcwd()
+                # Get a list of all files in the current directory
+                files = os.listdir(current_directory)
+                # Iterate over the files and delete them one by one
+                for file_name in files:
+                    file_path = os.path.join(current_directory, file_name)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+        else:
+            break
 
 
 if __name__ == '__main__':
