32d96a4bd18728b2b742cf4217052b1770fbd507
xiangyan93 <yan.xiang@duke.edu>
2023-06-03 22:12:21 -0400

    add features: free energy calculation using OPES_METAD.

--- scripts/BimoleculeMetaD.py
+++ scripts/BimoleculeMetaD.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 from rdkit import Chem
 import parmed as pmd
+from copy import deepcopy
 from simutools.forcefields.amber import AMBER
 from simutools.simulator.gromacs.gromacs import GROMACS
 from simutools.simulator.plumed import PLUMED
@@ -17,62 +18,102 @@ from simutools.utils import execute
 
 
 class CommonArgs(Tap):
+    """This script conduct OPES_METAD between two molecules. And output their binding free energy."""
     save_dir: str
     """the output directory."""
-    smiles: str = None
-    """SMILES of the molecule."""
-    dihedral_no_force: List[int] = []
-    """dihedral index without potential"""
-    dihedral_with_force: List[int] = []
-    """dihedral index with potential"""
+    smiles: Tuple[str, str] = None
+    """SMILES of the molecules."""
+    res_name: List[str] = None
+    """"""
+    solvent: Literal['vaccum', 'water']
+    """Simulation solvent."""
+    top: List[str] = None
+    """The top files of the two molecules of interested. (Alternative input of SMILES)"""
+    gro: List[str] = None
+    """The gro files of the two molecules of interested. (Alternative input of SMILES)"""
     ntmpi: int = None
     """number of MPI threads for gmx"""
     ntomp: int = None
     """number of OpenMP threads for gmx"""
-    n_iter: int = 1
-    """"""
-    solvent: Literal['vaccum', 'water']
-    """"""
-    top: List[str]
-    """"""
-    gro: List[str]
-    """"""
 
     @property
     def charge(self):
-        mol = Chem.MolFromSmiles(self.smiles)
-        total_formal_charge = sum([atom.GetFormalCharge() for atom in mol.GetAtoms()])
-        return total_formal_charge
+        return [sum([atom.GetFormalCharge() for atom in Chem.MolFromSmiles(self.smiles[0]).GetAtoms()]),
+                sum([atom.GetFormalCharge() for atom in Chem.MolFromSmiles(self.smiles[1]).GetAtoms()])]
+
+    @property
+    def SameMol(self):
+        if self.smiles[0] == self.smiles[1]:
+            return True
+        else:
+            return False
 
     def process_args(self) -> None:
-        assert len(self.gro) == 2
-        assert len(self.top) == 2
+        if self.smiles is not None:
+            assert self.top is None
+            assert self.top is None
+            assert len(self.smiles) in [1, 2]
+        else:
+            assert len(self.gro) == 2
+            assert len(self.top) == 2
 
 
 def main(args: CommonArgs):
     cd_and_mkdir(args.save_dir)
     gmx = GROMACS(gmx_exe_mdrun='gmx')
+    amber = AMBER()
     plumed = PLUMED(plumed_exe='plumed')
-
+    # skip energy minimization if it is finished
     if not os.path.exists('em.gro'):
-        ff = pmd.load_file(f'../{args.top[0]}', xyz=f'../{args.gro[0]}')
-        ff.strip(f':SOL')
-        ff += pmd.load_file(f'../{args.top[1]}', xyz=f'../{args.gro[1]}')
-        ff.save(f'bimolecule.top', overwrite=True)
-        ff.strip(f':SOL')
-        ff.save(f'bimolecule.gro', overwrite=True)
-        gmx.insert_molecules(f'bimolecule.gro', outgro='output.gro', box='5.0 5.0 5.0')
+        if args.smiles is None:
+            ff = pmd.load_file(f'../{args.top[0]}', xyz=f'../{args.gro[0]}')
+            ff.strip(f':SOL')
+            ff += pmd.load_file(f'../{args.top[1]}', xyz=f'../{args.gro[1]}')
+            ff.save(f'bimolecule.top', overwrite=True)
+            ff.strip(f':SOL')
+            ff.save(f'bimolecule.gro', overwrite=True)
+        else:
+            amber.build(args.smiles[0], 'mol1', charge=args.charge[0], gromacs=True, tip3p=False,
+                        resName=args.res_name[0])
+            gmx.fix_charge(f'mol1.top')
+            ff = pmd.load_file(f'mol1.top', xyz=f'mol1.gro')
+            if not args.SameMol:
+                amber.build(args.smiles[1], 'mol2', charge=args.charge[1], gromacs=True, tip3p=False,
+                            resName=args.res_name[1])
+                gmx.fix_charge(f'mol2.top')
+                ff += pmd.load_file(f'mol2.top', xyz=f'mol2.gro')
+            else:
+                new_ff = deepcopy(ff)
+                for atom in new_ff.atoms:
+                    atom.xx += 25.0
+                    atom.xy += 25.0
+                    atom.xz += 25.0
+                ff += new_ff
+            ff.save(f'bimolecule.gro', overwrite=True)
+            ff += pmd.load_file(f'{TEMPLATE_DIR}/tip3p.top', xyz=f'{TEMPLATE_DIR}/tip3p.gro')
+            ff.save(f'bimolecule.top', overwrite=True)
 
+        gmx.insert_molecules(f'bimolecule.gro', outgro='output.gro', box='5.0 5.0 5.0')
         if args.solvent == 'water':
             gmx.solvate('output.gro', top=f'bimolecule.top', outgro='initial.gro')
         else:
             shutil.copyfile('output.gro', 'initial.gro')
             ff.save(f'bimolecule.top', overwrite=True)
 
-        plumed.generate_dat_from_template('bimolecule.dat', output='plumed.dat', group1=f'1-{len(ff.residues[0].atoms)}',
-                                          group2=f'{len(ff.residues[0].atoms) + 1}-'
-                                                 f'{len(ff.residues[0].atoms) + len(ff.residues[1].atoms)}',
-                                          biasfactor=100)
+        if args.charge[0] != 0:
+            assert ff.residues[1].name in ['NA', 'CL']
+            n = abs(args.charge[0])
+            plumed.generate_dat_from_template('bimolecule.dat', output='plumed.dat',
+                                              group1=f'1-{len(ff.residues[0].atoms)}',
+                                              group2=f'{len(ff.residues[0].atoms) + n + 1}-'
+                                                     f'{len(ff.residues[0].atoms) + n + len(ff.residues[1 + n].atoms)}',
+                                              biasfactor=100)
+        else:
+            plumed.generate_dat_from_template('bimolecule.dat', output='plumed.dat',
+                                              group1=f'1-{len(ff.residues[0].atoms)}',
+                                              group2=f'{len(ff.residues[0].atoms) + 1}-'
+                                                     f'{len(ff.residues[0].atoms) + len(ff.residues[1].atoms)}',
+                                              biasfactor=100)
 
         gmx.generate_mdp_from_template('t_em.mdp', mdp_out=f'em.mdp', dielectric=1.0)
         gmx.grompp(gro='initial.gro', mdp='em.mdp', top=f'bimolecule.top', tpr='em.tpr', maxwarn=1)
